e0a5ccc25c35a1df248808b8bab4ec6106ebe04c
Abhijeet Singh <abhijeet.singh@razorpay.com>
2022-04-14 15:47:38 +0530

    feat: add Recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Sandwich',
+      'Grilled Veg Sandwich.',
+      'https://cdn.pixabay.com/photo/2021/05/23/22/27/sandwich-6277424_1280.png'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
