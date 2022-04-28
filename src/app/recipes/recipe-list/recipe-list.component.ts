22813ea8338f79bf190fd8a006aa1dfbd64202a9
Abhijeet Singh <abhijeet.singh@razorpay.com>
2022-04-28 10:04:31 +0530

    feat: Make recipe details work

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,6 +8,8 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe(
       'Sandwich',
@@ -15,13 +17,17 @@ export class RecipeListComponent implements OnInit {
       'https://cdn.pixabay.com/photo/2021/05/23/22/27/sandwich-6277424_1280.png'
     ),
     new Recipe(
-      'Sandwich',
-      'Grilled Veg Sandwich.',
-      'https://cdn.pixabay.com/photo/2021/05/23/22/27/sandwich-6277424_1280.png'
+      'Burger',
+      'Aloo Tikki Burger.',
+      'https://cdn.pixabay.com/photo/2017/02/05/21/06/burger-2041192_1280.jpg'
     ),
   ];
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
