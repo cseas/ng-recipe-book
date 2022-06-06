353b8e763b097360844634ab7da52b8235a9a5a9
Abhijeet Singh <abhijeet.singh@razorpay.com>
2022-06-07 03:51:04 +0530

    feat: add ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
@@ -7,13 +8,19 @@ export class RecipeService {
   private recipes: Recipe[] = [
     new Recipe(
       'Sandwich',
-      'Grilled Veg Sandwich.',
-      'https://cdn.pixabay.com/photo/2021/05/23/22/27/sandwich-6277424_1280.png'
+      'A super tasty Grilled Veg Sandwich.',
+      'https://cdn.pixabay.com/photo/2021/05/23/22/27/sandwich-6277424_1280.png',
+      [new Ingredient('Bread', 2), new Ingredient('Cheese Slices', 1)]
     ),
     new Recipe(
       'Burger',
-      'Aloo Tikki Burger.',
-      'https://cdn.pixabay.com/photo/2017/02/05/21/06/burger-2041192_1280.jpg'
+      'Vegetarian Aloo Tikki Burger with fries.',
+      'https://cdn.pixabay.com/photo/2017/02/05/21/06/burger-2041192_1280.jpg',
+      [
+        new Ingredient('Burger Buns', 2),
+        new Ingredient('Patty', 1),
+        new Ingredient('French Fries', 20),
+      ]
     ),
   ];
 
